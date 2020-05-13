492735545fc762f0c81c6b62355113b59a4e8851
KareemAshraf33 <62017004+KareemAshraf33@users.noreply.github.com>
2020-05-13 11:36:10 +0200

    Add Project Files
    Add Project Files and Database and Models

---
+++ Online-Ordering-Food/Online-Ordering-Food/Controllers/ResturantController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Online_Ordering_Food.Models;
+
+namespace Online_Ordering_Food.Controllers
+{
+    public class ResturantController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
